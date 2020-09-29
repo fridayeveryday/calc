0b88661692b438f31bf51f242bc256ee99a7c0d9
Pavel <50109083+fridayeveryday@users.noreply.github.com>
2020-09-29 17:47:36 +0400

    Add files via upload

--- Assets/ButtonClick.cs
+++ Assets/ButtonClick.cs
@@ -1,53 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Data;
+using System.Linq.Expressions;
 
 public class ButtonClick : MonoBehaviour
 {
 
     public Button butt;
-    public Text text;
-/*    public string calc1;
-    public string x1;*/
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text totalRes;
 
     public void click()
     {
-        /* Debug.Log(butt.GetComponent<Text>().text)*/
-        string st = butt.GetComponentInChildren<Text>().text;
-/*        string str = butt.transform.FindChild("Text").GetComponent<Text>.text;
-*/        text.text += st;
+        totalRes.text += butt.GetComponentInChildren<Text>().text;
     }
 
     public void clickOnEquals()
     {
-        
         DataTable dt = new DataTable();
-        string res = (dt.Compute(text.text, "")).ToString();
-        click();
-        text.text = res;
-
+        string st = totalRes.text.Replace(',', '.');
+        string res = dt.Compute(st, null).ToString();
+        totalRes.text = res;
     }
-
 
     public void clickOnClear()
     {
-        text.text = text.text.Substring(0, text.text.Length - 1);
+        totalRes.text = totalRes.text.Substring(0, totalRes.text.Length - 1);
     }
 
     public void clickOnClearAll()
     {
-        text.text = "";
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        totalRes.text = "";
     }
 }
