a1591348aab33904ec23f17d154991058ce2ea93
fridayeveryday <p7ll7p@gmail.com>
2020-09-29 09:09:49 +0400

    1st commit

---
+++ Assets/Calc.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calc : MonoBehaviour
+{
+    public Text textOfResult;
+    public Button button;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void clickOnButton(string val)
+    {
+
+        button.GetComponent<Text>().text = "";
+    }
+
+
+    public void clickOnDigit(double val)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
